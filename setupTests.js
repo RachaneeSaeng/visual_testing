adc42e77608cbe1783d4bf0b2174a2959dd4f35b
apple-tw <rachanee.s@thoughtworks.com>
2022-06-21 10:15:39 +0700

    Use ssim machanism

--- setupTests.js
+++ setupTests.js
@@ -3,15 +3,22 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
-import { toMatchImageSnapshot } from "jest-image-snapshot";
+import { configureToMatchImageSnapshot } from "jest-image-snapshot";
 import { setDefaultOptions } from "jsdom-screenshot";
+import "@testing-library/jest-dom/extend-expect";
 
 // Docker requires --no-sandbox to be able to run the tests
 setDefaultOptions({
-  launch: { args: ["--no-sandbox"] }
+  launch: { args: process.env.CI === "true" ? ["--no-sandbox"] : [] }
 });
 
 // Make sure jest has enough time to capture the screenshots
-jest.setTimeout(15000);
+jest.setTimeout(10000);
 
+const toMatchImageSnapshot = configureToMatchImageSnapshot({
+  comparisonMethod: "ssim",
+  customDiffConfig: {
+    ssim: "fast" //'bezkrovny'
+  }
+});
 expect.extend({ toMatchImageSnapshot });
